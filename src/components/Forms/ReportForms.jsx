4eb1e8fb9bf1322b79f3829c305a7f5a4c2a2632
Prakhar <prakharb56@gmail.com>
2023-09-15 23:13:20 +0530

    added for and rating

--- src/components/Forms/ReportForms.jsx
+++ src/components/Forms/ReportForms.jsx
@@ -1,40 +1,97 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useState } from 'react';
-import { View } from 'react-native';
-import { Button, SegmentedButtons, TextInput } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Button, SegmentedButtons, Snackbar, TextInput } from 'react-native-paper';
 import { useTicketStore } from '../../store';
 import MapWrapper from '../maps/MapWrapper';
 
 const ReportForms = () => {
   const { navigate } = useNavigation();
   const { createTicket } = useTicketStore();
-
+  const [visible, setVisible] = useState(false);
   const [formData, setFormData] = useState({
+    rating: 0,
+    for: 0,
     type: 0,
     message: null
   });
 
+  const onDismissSnackBar = () => setVisible(false);
+
   return (
     <>
       <SegmentedButtons
-        value={formData.type}
-        onValueChange={(type) => setFormData({ ...formData, type })}
+        value={formData.for}
+        onValueChange={(data) => {
+          setFormData({ ...formData, for: data });
+
+          if (data === 1) {
+            setVisible(true);
+          } else {
+            setVisible(false);
+          }
+        }}
         buttons={[
           {
             value: 0,
-            label: 'Non Anonymous',
+            label: 'Self',
             showSelectedCheck: true
           },
           {
             value: 1,
-            label: 'Anonymous',
+            label: 'Other',
             showSelectedCheck: true
           }
         ]}
       />
+
+      {!formData.for && (
+        <SegmentedButtons
+          value={formData.type}
+          onValueChange={(type) => setFormData({ ...formData, type })}
+          buttons={[
+            {
+              value: 0,
+              label: 'Non Anonymous',
+              showSelectedCheck: true
+            },
+            {
+              value: 1,
+              label: 'Anonymous',
+              showSelectedCheck: true
+            }
+          ]}
+        />
+      )}
+      <Snackbar
+        visible={visible}
+        onDismiss={onDismissSnackBar}
+        action={{
+          label: 'Undo'
+        }}
+      >
+        You will be reporting as Non-Anonymous
+      </Snackbar>
       <View style={{ height: '50%' }}>
         <MapWrapper></MapWrapper>
       </View>
+      <SegmentedButtons
+        value={formData.rating}
+        onValueChange={(rating) => {
+          setFormData({ ...formData, rating });
+        }}
+        buttons={[
+          {
+            value: 0,
+            label: 'Low'
+          },
+          { value: 1, label: 'medium' },
+          {
+            value: 2,
+            label: 'high'
+          }
+        ]}
+      />
       <TextInput
         label="Report your incident here"
         multiline
@@ -56,5 +113,11 @@ const ReportForms = () => {
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center'
+  }
+});
 
 export default ReportForms;
