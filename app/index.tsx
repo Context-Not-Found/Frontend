ac4f399547cccd97f330680d10cd3c6d82e59a3e
Drish-xD <drish.xd@gmail.com>
2023-10-26 02:34:55 +0530

    init: safeHer@v2

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { H1, Paragraph, YStack } from "tamagui";
+
+import { MyStack } from "../components/MyStack";
+
+export default function Home() {
+  return (
+    <MyStack>
+      <YStack space="$4" maxWidth={600}>
+        <H1 textAlign="center">Welcome to Tamagui.</H1>
+        <Paragraph textAlign="center">
+          Here&apos;s a basic starter to show navigating from one screen to
+          another.
+        </Paragraph>
+      </YStack>
+    </MyStack>
+  );
+}
